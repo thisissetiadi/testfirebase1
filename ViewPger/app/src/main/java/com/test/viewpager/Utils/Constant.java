9a14864dba161f0efc635437fe430a65773e779b
Adhi <setiadizainalmuttaqin@gmail.com>
2020-05-21 14:50:55 +0700

    Ini adalah pesan commit

--- ViewPger/app/src/main/java/com/test/viewpager/Utils/Constant.java
+++
@@ -1,8 +0,0 @@
-package com.test.viewpager.Utils;
-
-public class Constant {
-    public static String PLAY_MODE = "playmode";
-    public static final int PLAY_MODE_CHILD = 2;
-    public static final String ARG_CHILD_ID = "arg_child_id";
-
-}
